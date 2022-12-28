19230813adb0fd92f9193bc98a49aad617c6d35e
canberkardic <cnbrk371@gmail.com>
2022-12-29 00:58:44 +0300

    Preference Animation Added

--- src/app/components/another/another.component.ts
+++ src/app/components/another/another.component.ts
@@ -1,36 +1,60 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Input } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 import { Output } from '@angular/core';
 import { Component } from '@angular/core';
 import { DashboardComponentDec, IDashboardComponentActions } from 'src/app/services/decorator/decorator-helpter';
-import { myAnimation } from 'src/app/shared/common-animations';
 
+const enterTransition = transition(':enter', [
+  style({
+    opacity: 0
+  }),
+  animate('1s ease-in', style({
+    opacity: 1
+  }))
+]);
 
+const leaveTrans = transition(':leave', [
+  style({
+    opacity: 1
+  }),
+  animate('1s ease-out', style({
+    opacity: 0
+  }))
+])
+
+export const fadeIn = trigger('fadeIn', [
+  enterTransition
+]);
+
+export const fadeOut = trigger('fadeOut', [
+  leaveTrans
+]);
 
 @DashboardComponentDec({
   desc: 'AnotherDesc',
   icon: './assets/sidenav-icons/A.png',
   componentName: 'AnotherComp',
   component: AnotherComponent,
-  animations: [
-    trigger('popOverState', [
-      state('show', style({
-        opacity: 1
-      })),
-      state('hide', style({
-        opacity: 0
-      })),
-      transition('show => hide', animate('600ms ease-out')),
-      transition('hide => show', animate('1000ms ease-in'))
-    ])
-  ]
 })
 
 @Component({
   selector: 'app-another',
   templateUrl: './another.component.html',
   styleUrls: ['./another.component.scss'],
+  animations: [
+    fadeIn,
+    fadeOut,
+    trigger('fadeInOut', [
+      transition(':enter', [   // :enter is alias to 'void => *'
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 }))
+      ]),
+      transition(':leave', [   // :leave is alias to '* => void'
+        animate(500, style({ opacity: 0 }))
+      ])
+    ])
+  ],
 })
 export class AnotherComponent implements IDashboardComponentActions {
   kind: string = "data";
@@ -50,19 +74,6 @@ export class AnotherComponent implements IDashboardComponentActions {
 
     this.showPreferences = !this.showPreferences;
 
-  }
-
-  show = false;
-
-
-
-  get stateName() {
-    return this.show ? 'show' : 'hide'
-  }
-
-
-  toggle() {
-    this.show = !this.show;
   }
 
 }
